82298de2993325efc2db71f8b49ca95a0fd983ee
Ayesha141-hub <ayeshasajan202455@gmail.com>
2025-03-26 15:17:11 +0500

    practice

---
+++ appoinment.js
@@ -0,0 +1,10 @@
+// Auto typing...
+var typed = new Typed('#brand_name', {
+    strings: ['<b>ZARA THERAPY CLINIC</b>'],
+    typeSpeed: 200,    // Typing speed
+    backSpeed: 50,     // Deleting speed
+    backDelay: 2000,   // 2 sec tak text dikhne ke baad delete hoga
+    fadeOut: true,     // Soft fade-out effect ke saath remove hoga
+    loop: true,        // Loop enable karna
+    showCursor: false  // Cursor hide karna
+});
